7f0c57863daf5ca8f2000ef21bc448c299ea1ce6
olegrewko <olegrewko@gmail.com>
2023-09-02 18:17:19 +0300

    begin

---
+++ source/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('.main-nav__toggle').on('click', function name() {
+    $('.main-nav__wrapper').toggleClass('main-nav__wrapper--active');
+
+
+  });
+});
+// $(function () {
+//   $('.menu__btn').on('click', function name() {
+//     $('.menu-mobile__list').toggleClass('menu-mobile__list--active');
+
+
+//   });
+// });
